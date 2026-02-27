b885d14cdd35e10e4db8a03903d67f2ce02683fe
bbthecat <bbthecat.1234@gmail.com>
2026-02-28 03:16:31 +0700

    test

---
+++ init_scripts/02_insert_mockdata.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO employees (first_name, last_name, department) VALUES ('Somchai', 'Jaidee', 'IT');
+INSERT INTO employees (first_name, last_name, department) VALUES ('Somsri', 'Rakdee', 'HR');
+COMMIT;
