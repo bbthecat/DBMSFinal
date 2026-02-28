1427efc8b53abb3dc032c4a5745da08df73fdbc6
patipanpan-sys <patipan.pan@kkumail.com>
2026-02-28 18:28:39 +0700

    Jame

--- Table/Supplier.sql
+++ Table/Supplier.sql
@@ -4,10 +4,3 @@ CREATE TABLE Supplier(
     Contract VARCHAR2(100),
     CONSTRAINTS Supplier_pk PRIMARY KEY (Supplier_ID)
 );
-
-INSERT INTO Supplier VALUES ('S01','Sara_supply','Sara_supple co.');
-
-SELECT * FROM SUPPLIER;
-
-UPDATE SUPPLIER SET CONTRACT = 'Sara_supply co.'
-WHERE CONTRACT = 'Sara_supple co.';
